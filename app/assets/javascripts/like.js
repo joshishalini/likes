32447d5b82abe88bb554a8538da05fb9a9af23b4
Shalini Joshi <shalinijoshi1996@gmail.com>
2018-06-10 22:57:58 +0530

    w3socket like

--- app/assets/javascripts/like.js
+++ app/assets/javascripts/like.js
@@ -10,11 +10,16 @@ $(document).on('click','#like',function(){
 }); 
 
 $(document).ready(function(){
-	console.log(w3sockets_key);
-	console.log("current_user", current_user);
   w3socket = new W3sockets(w3sockets_key);
   channel = w3socket.subscribe('like');
-  channel.bindEvent('event-name-'+current_user, function() {
+  channel.bindEvent('event-name-'+current_user, function(from) {
   	console.log("111111111111111111111111");
+  	console.log(from);
+  	open_popup(from);
   });
 });
+
+
+function open_popup(from){
+	$('.popup').append('<div class="alert alert-warning alert-dismissible fade show" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><strong>Hey ',from.from,' liked your post.</strong></div>');
+}
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,16 @@
 class LikesController < ApplicationController
 	def save_like
-		p post_id = params[:id]
-		p likes_params = { 
+		post_id = params[:id]
+		@post = Post.find(params[:id])
+		p @post.user.id
+		user = current_user.id.to_s
+		likes_params = { 
 				user_id: current_user.id,
 				post_id: post_id
 			}
 		@likes = Like.create(likes_params)
 		unless @likes.errors.present?
-			p @response = LiveNotifications::W3socket.push('like', 'event-name-'+current_user.id, current_user.id)
-			p "-------------------"
+			p@response = LiveNotifications::W3socket.push('like', 'event-name-'+user, {from: current_user.email, to: @post.user.id})
 			render json: @response
 
 		else
